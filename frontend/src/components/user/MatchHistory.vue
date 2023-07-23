034e0f12226fbdc02174f725d15eb546f226a3d7
chelmerd <chelmerd@student.42wolfsburg.de>
2023-07-23 15:36:17 +0000

    add component MatchList

--- frontend/src/components/user/MatchHistory.vue
+++ frontend/src/components/user/MatchHistory.vue
@@ -1,44 +1,10 @@
 <script setup lang="ts">
-import MatchItemVue from '@/components/match/MatchItem.vue'
-
-interface MatchResult {
-  id: number;
-  completed: boolean;
-  start: Date;
-  end: Date;
-  players: {
-    id: number;
-    score: number;
-    name: string;
-    email: string;
-  }[];
-}
-
-const match: MatchResult = {
-  id: 1,
-  completed: true,
-  start: new Date('2023-07-08T13:47:45.386Z'),
-  end: new Date('2023-07-08T14:47:45.386Z'),
-  players: [
-    {
-      id: 1,
-      score: 4,
-      email: 'alice@prisma.io',
-      name: 'Alice'
-    },
-    {
-      id: 2,
-      score: 0,
-      email: 'bob@prisma.io',
-      name: 'Bob'
-    }
-  ]
-}
+import MatchListVue from '@/components/match/MatchList.vue';
 </script>
 
 <template>
   <div class="text-center">
-    this is the Matchhistory tap
+    this is the MatchHistory tap
   </div>
-  <MatchItemVue :match="match" />
+  <MatchListVue />
 </template>
