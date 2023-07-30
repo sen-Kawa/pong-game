3160dd7f4f5299ab8a876a25c1c1ffc5b7635e64
chelmerd <chelmerd@student.42wolfsburg.de>
2023-07-30 15:55:47 +0000

    remove user module from games module

--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { MatchService } from './match.service'
 import { MatchController } from './match.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
-import { UsersModule } from 'src/users/users.module'
 
 @Module({
   controllers: [MatchController],
   providers: [MatchService],
-  imports: [PrismaModule, UsersModule]
+  imports: [PrismaModule]
 })
 export class GamesModule {}
