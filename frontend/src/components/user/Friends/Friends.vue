54e591517519f4a93d7b2b73b9e47ad87bc9e80a
olin_tp_ar <universalexperimental@protonmail.com>
2023-08-02 18:41:49 +0200

    adding friends.vue

---
+++ frontend/src/components/user/Friends/Friends.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="friends">
+		<h1>Friends</h1>
+		<ul v-if="friends.length">
+			<li v-for="friend in friends" :key="friend">{{friend}}<button>Remove Friend</button></li>
+		</ul>
+		<div v-else>
+			You dont have friends yet!
+		</div>
+		<Button text="Add Friend" color="LightGray" />
+    </div>
+</template>
+
+<script>
+import Button from '../../Button.vue'
+export default {
+	data() {
+		return {
+			friends: [1, 2],
+		};
+	},
+	components: {
+		Button
+	},
+}
+</script>
+
+<style scoped>
+	div.friends {
+		#display: flex;
+		#justify-content: space-between;
+		#align-items: center;
+	}
+</style>
