e5eda4205344a26e4e8b10d929811bf86b46fa73
olin_tp_ar <universalexperimental@protonmail.com>
2023-08-02 15:32:40 +0200

    button component and buttons in friends component

--- frontend/src/components/user/Friends.vue
+++ frontend/src/components/user/Friends.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="text-center">
-      this is the friends tap
+    <div class="friends">
+		<h1>Friends</h1>
+		<Button text="Show Friends List" color="LightGray" />
+		<Button text="Add Friend" color="LightGray" />
+		<Button text="Delete Friend" color="LightGray" />
     </div>
 </template>
+
+<script>
+import Button from '../Button.vue'
+export default {
+	name: 'FriendsTab',
+	components: {
+		Button
+	},
+}
+</script>
+
+<style scoped>
+	div.friends {
+		#display: flex;
+		#justify-content: space-between;
+		#align-items: center;
+	}
+</style>
