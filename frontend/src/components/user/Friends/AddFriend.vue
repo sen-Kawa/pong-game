6427426309e69107ee74611e2e36a81c88995633
olin_tp_ar <universalexperimental@protonmail.com>
2023-08-02 20:20:36 +0200

    simple add friend without fetch yet

--- frontend/src/components/user/Friends/AddFriend.vue
+++ frontend/src/components/user/Friends/AddFriend.vue
@@ -2,7 +2,25 @@
 	<div>
 		<h2>Add Friend</h2>
 		<form @submit.prevent="addFriend">
-			<input type="text" placeholder="Input name">
+			<input v-model="searchTerm" type="text" placeholder="Input name">
+			<button type="submit">Add</button>
 		</form>
+		<h3 v-show="friendAdded">Friend added!</h3>
 	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			searchTerm: '',
+			friendAdded: false,
+		};
+	},
+	methods: {
+		async addFriend() {
+			this.friendAdded = !this.friendAdded
+		}
+	}
+}
+</script>
