a553c5a8e80faf9b8380c54e6641f33b5e0357f1
chelmerd <chelmerd@student.42wolfsburg.de>
2023-08-03 16:11:13 +0200

    add error message display on failed fetch

--- frontend/src/components/match/MatchList.vue
+++ frontend/src/components/match/MatchList.vue
@@ -2,16 +2,27 @@
 import { onMounted, ref } from 'vue';
 import MatchItemVue from './MatchItem.vue';
 import type { MatchResult } from '@/types/match';
+import { AxiosError } from 'axios'
 
 import MatchService from '@/services/MatchService'
 
 const matches = ref([] as MatchResult[])
+let loading = ref(false);
+let error = ref("")
 
 onMounted(async () => {
   const baseUrl = import.meta.env.VITE_BACKEND_SERVER_URI
   const matchService: MatchService = new MatchService(baseUrl);
 
-  matches.value = await matchService.getMatchHistory();
+  loading.value = true
+  try {
+    matches.value = await matchService.getMatchHistory();
+  } catch (e) {
+    console.debug(e)
+    if (e instanceof AxiosError)
+      error.value = e.message + ' ' + e.response?.statusText // TODO: provide nicer message
+  }
+  loading.value = false
 });
 
 </script>
@@ -19,7 +30,15 @@ onMounted(async () => {
 <template>
   <div class="match-list">
     <h2>Match List</h2>
-    <ul>
+    <div v-if="loading" class="loading">
+      Loading...
+    </div>
+
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+
+    <ul v-if="matches" class="content">
       <MatchItemVue v-for="match in matches" :key="match.id" :match="match" />
     </ul>
   </div>
