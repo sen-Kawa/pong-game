cabdbd47876fbc155cec2d271bd5c077718479fd
hrings <hrings@student.42.fr>
2023-08-09 17:20:44 +0200

    fixed some lint erros on frontend, beside multi-word-error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,8 +9,7 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
+
 </script>
 
 <style>
--- frontend/src/helper.js
+++
@@ -1,3 +0,0 @@
-export const server = {
-  baseURL: process.env.VUE_APP_BACKEND_SERVER_URI
-}
