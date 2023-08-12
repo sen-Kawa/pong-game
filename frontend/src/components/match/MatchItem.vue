fa756b5ce51f757a1cdec89fd9cea1df4837da9d
chelmerd <chelmerd@student.42wolfsburg.de>
2023-08-12 18:11:20 +0200

    add placeholder for empty spot in a match

--- frontend/src/components/match/MatchItem.vue
+++ frontend/src/components/match/MatchItem.vue
@@ -40,8 +40,10 @@ const highscore = computed(() => {
     <h3>Match #{{ match.id }}</h3>
     <p v-if="match.players">
       <!-- TODO: highlight the winner -->
-      <span v-if="match.players[0]">{{ match.players[0].name }}</span> vs <span v-if="match.players[1]">{{
-        match.players[1].name }}</span>
+      <span v-if="match.players[0]">{{ match.players[0].name }}</span>
+      vs
+      <span v-if="match.players[1]">{{ match.players[1].name }}</span>
+      <span v-else>???</span>
     </p>
     <p v-if="match.end">{{ match.players[0].score }} : {{ match.players[1].score }}</p>
     <p id="match-time-since-end">{{ timeSinceEnd }}</p>
