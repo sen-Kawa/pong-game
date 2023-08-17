0ab6adae7c4ede5003a63b5d48ad8edeaef2f7e8
Frederic Henrichs <f.henrich@students.42wolfsburg.de>
2023-08-17 07:11:10 +0200

    add GameComponent.

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="text-center">this is the game tap</div>
   <button @click="createNewGame">New Game</button>
-  <MatchList :initial-scope="Scope.inProgress" />
+  <GameComponent v-if="inGame"/>
+  <MatchList v-else :initial-scope="Scope.inProgress" />
 </template>
 
 <script setup lang="ts">
 import MatchService, { Scope } from '@/services/MatchService'
 import MatchList from '@/components/match/MatchList.vue'
+import { ref } from 'vue';
+import GameComponent from '@/components/match/GameComponent.vue';
 
 const baseUrl = import.meta.env.VITE_BACKEND_SERVER_URI
 const matchService: MatchService = new MatchService(baseUrl)
+const inGame = ref(false);
 
 async function createNewGame() {
   // TODO: indicate progress, error
@@ -17,5 +21,6 @@ async function createNewGame() {
   console.debug(match)
 
   await matchService.startMatch(match.data.id)
+  inGame.value = true;
 }
 </script>
