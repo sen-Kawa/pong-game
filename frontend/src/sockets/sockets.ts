47ef7cfe00e2c6399d6d1648d70040a7ee000be0
Frederic Henrichs <f.henrich@students.42wolfsburg.de>
2023-08-18 12:36:49 +0200

    Fix arguments for callback function.

--- frontend/src/sockets/sockets.ts
+++ frontend/src/sockets/sockets.ts
@@ -1,27 +1,25 @@
 import { io, Socket } from 'socket.io-client'
 
+type Fof<T extends any[]> = (...args: T) => void;
+
 class SockManager {
 	socket: Socket;
 	options: any;
 	constructor() {
+		// FIXME: Somehow get dynamically the url of the server.
 		this.socket = io('ws://localhost:3000', {
 			withCredentials: true
 		});
-		this.socket.on("hello", () => console.log("Hello World"));
 	}
 
-	on(ev: string, cb: (args?: any[]) => void) {
+	// We need to "Omit" the type of the callback because otherwise Typescript would not
+	// allow us to spread the dictionary we are getting from the socket.
+	on(ev: string, cb: Fof<any[]>) {
 		this.socket.on(ev, cb);
 	}
 
 	emit(ev: string, args?: any[]) {
 		this.socket.emit(ev, args);
-	}
-
-	setCookies(cookies: string) {
-		this.socket.io.opts.extraHeaders = {
-			cookie: cookies
-		}
 	}
 }
 
