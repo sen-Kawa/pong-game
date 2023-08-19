f4e473ee3c56a55ebe33eb516583e4ebe0b30107
chelmerd <chelmerd@student.42wolfsburg.de>
2023-08-19 15:23:27 +0200

    change main layout - add main element between header and footer - add color to footer - add ability to scroll the main content

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,18 +1,14 @@
 <template>
-  <div v-if="authStore.isLoggedIn">
-    <nav id="header">
-      <div class="menu-item"><router-link to="/">Home</router-link></div>
-      <div class="menu-item"><router-link to="/game">Game</router-link></div>
-      <div class="menu-item"><router-link to="/leader">Leaderboard</router-link></div>
-      <User title="User" />
-      <router-link @click="authStore.logout()" class="menu-item" to="/">Logout</router-link>
-    </nav>
-  </div>
-  <div v-if="!authStore.isLoggedIn">
-    <nav id="header">
-      <div class="menu-item"><router-link to="/">Home</router-link></div>
-    </nav>
-  </div>
+  <nav id="header" v-if="authStore.isLoggedIn">
+    <div class="menu-item"><router-link to="/">Home</router-link></div>
+    <div class="menu-item"><router-link to="/game">Game</router-link></div>
+    <div class="menu-item"><router-link to="/leader">Leaderboard</router-link></div>
+    <User title="User" />
+    <router-link @click="authStore.logout()" class="menu-item" to="/">Logout</router-link>
+  </nav>
+  <nav id="header" v-else>
+    <div class="menu-item"><router-link to="/">Home</router-link></div>
+  </nav>
 </template>
 
 <script setup lang="ts">
