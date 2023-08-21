84ee81f23d642aed4cb86382775addba0ee6c3e4
chelmerd <chelmerd@student.42wolfsburg.de>
2023-08-21 22:44:20 +0200

    move match fetching to match history

--- frontend/src/components/match/MatchList.vue
+++ frontend/src/components/match/MatchList.vue
@@ -1,66 +1,22 @@
 <script setup lang="ts">
-import type { MatchResult } from '@/types/match'
-import { AxiosError } from 'axios'
-import { onMounted, ref, type PropType } from 'vue'
-import MatchItemVue from './MatchItem.vue'
-
-import MatchService, { Scope } from '@/services/MatchService'
-import LoadingIndicator from './LoadingIndicator.vue'
+import type { MatchResult } from '@/types/match';
+import { type PropType } from 'vue';
+import MatchItemVue from './MatchItem.vue';
 
 const props = defineProps({
-  initialScope: {
-    type: Number as PropType<Scope>,
+  matches: {
+    type: Array as PropType<MatchResult[]>,
     required: true
   }
-})
-const scope = ref(props.initialScope) // maybe emit an event and let the parent change it
-
-const matches = ref([] as MatchResult[])
-let loading = ref(false)
-let error = ref('')
-
-async function getMatches() {
-  console.debug('getMatches scope ' + scope.value)
-
-  const baseUrl = import.meta.env.VITE_BACKEND_SERVER_URI
-  const matchService: MatchService = new MatchService(baseUrl)
-
-  loading.value = true
-  try {
-    if (scope.value === Scope.inProgress) matches.value = await matchService.getInProgressMatches()
-    else matches.value = await matchService.getMatchHistory(scope.value)
-  } catch (e) {
-    console.debug(e)
-    if (e instanceof AxiosError) error.value = e.message + ' ' + e.response?.statusText // TODO: provide nicer message
-  }
-  loading.value = false
-}
-
-function toggleScope() {
-  matches.value = []
-  if (scope.value === Scope.global) scope.value = Scope.personal
-  else if (scope.value === Scope.personal) scope.value = Scope.global
-  getMatches()
-}
-
-onMounted(async () => {
-  console.debug('onMounted')
-  getMatches()
 })
 </script>
 
 <template>
-  <h2>{{ Scope[scope] }} Match List</h2>
-  <LoadingIndicator :is-loading="loading" :error="error">
-
-    <button @click="toggleScope" v-if="scope !== Scope.inProgress">Switch Scope</button>
-
-    <div class="content">
-      <ul v-if="matches" class="match-list">
-        <MatchItemVue v-for="match in matches" :key="match.id" :match="match" />
-      </ul>
-    </div>
-  </LoadingIndicator>
+  <div class="content">
+    <ul v-if="matches" class="match-list">
+      <MatchItemVue v-for="match in matches" :key="match.id" :match="match" />
+    </ul>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/components/user/MatchHistory.vue
+++ frontend/src/components/user/MatchHistory.vue
@@ -1,11 +1,65 @@
 <script setup lang="ts">
-import { Scope } from '@/services/MatchService'
-import MatchList from '@/components/match/MatchList.vue'
+import MatchList from '@/components/match/MatchList.vue';
 import SearchMatch from '@/components/match/SearchMatch.vue';
+import MatchService, { Scope } from '@/services/MatchService';
+import type { MatchResult } from '@/types/match';
+import { onMounted, ref } from 'vue';
+import LoadingIndicator from '../match/LoadingIndicator.vue';
+
+interface Props {
+  initialScope: Scope
+}
+
+const props = withDefaults(defineProps<Props>(),
+  {
+    initialScope: Scope.global
+  })
+
+const scope = ref(props.initialScope)
+const loading = ref(false)
+const error = ref('')
+const matches = ref([] as MatchResult[])
+
+const baseUrl = import.meta.env.VITE_BACKEND_SERVER_URI
+const matchService: MatchService = new MatchService(baseUrl)
+
+async function getMatches() {
+  console.debug('getMatches scope ' + Scope[scope.value])
+
+  error.value = ''
+  loading.value = true
+  try {
+    matches.value = await matchService.getMatchHistory(scope.value)
+    await new Promise(resolve => setTimeout(resolve, 1000)); // TODO: remove debug delay
+    if (matches.value.length === 0)
+      error.value = "No Matches found!"
+  } catch (e: any) {
+    console.debug(e)
+    // if (e instanceof AxiosError)
+    error.value = "Error while loading matches: " + e?.message + ' ' + e?.response?.statusText // TODO: provide nicer message
+
+  }
+  loading.value = false
+}
+
+function toggleScope() {
+  matches.value = []
+  if (scope.value === Scope.global) scope.value = Scope.personal
+  else if (scope.value === Scope.personal) scope.value = Scope.global
+  getMatches()
+}
+
+onMounted(() => {
+  console.debug('onMounted MatchHistory')
+  getMatches()
+})
 </script>
 
 <template>
-  <div class="text-center">this is the MatchHistory tap</div>
+  <h2>{{ Scope[scope] }} MatchHistory</h2>
+  <button @click="toggleScope">Switch Scope</button>
+  <LoadingIndicator :is-loading="loading" :error="error">
+    <MatchList :matches="matches" />
+  </LoadingIndicator>
   <SearchMatch />
-  <MatchList :initialScope="Scope.personal" />
 </template>
