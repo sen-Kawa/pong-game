2f0ef81a5407a529b72a17c8e44b80ef1d10b4ed
mtrietz <Markus Trietz>
2023-09-19 13:49:32 +0200

    UPDATE test files

--- backend/src/statistics/statistics.service.spec.ts
+++ backend/src/statistics/statistics.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { StatisticsService } from './statistics.service';
+import { PrismaService } from 'src/prisma/prisma.service'
+import prisma from 'src/prisma/__mocks__/prisma'
+
+
+import { StatisticsService } from './statistics.service'; 
 
 describe('StatisticsService', () => {
   let service: StatisticsService;
 
+  jest.mock('src/prisma/prisma.service')
+
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StatisticsService],
+      providers: [PrismaService, StatisticsService],
     }).compile();
 
     service = module.get<StatisticsService>(StatisticsService);
