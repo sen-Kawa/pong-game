806ff7e124750a6fa668e466baf62c30ed535324
mtrietz <Markus Trietz>
2023-09-19 09:02:12 +0200

    Delete unnecessary imports in ProfileComp

--- frontend/src/components/user/ProfileComp.vue
+++ frontend/src/components/user/ProfileComp.vue
@@ -33,10 +33,8 @@
 <script setup lang="ts">
     import { ref } from 'vue'
     import { useAuthStore } from '../../stores/auth.js'
-    import Twofactor from './TwoFactor.vue'
-    import FileUpload from './FileUpload.vue'
-    const backendUrl = `${import.meta.env.VITE_BACKEND_SERVER_URI}`
 
+    const backendUrl = `${import.meta.env.VITE_BACKEND_SERVER_URI}`
     const authStore = useAuthStore()
     authStore.getuserProfile()
     const numberOfPlayers = ref<string>()
