b7f7e41632a88f85f2fbdd6539d75870d1a25131
mtrietz <Markus Trietz>
2023-09-19 13:52:47 +0200

    UPDATE test files

--- backend/src/statistics/statistics.service.spec.ts
+++ backend/src/statistics/statistics.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from 'src/prisma/prisma.service'
-import prisma from 'src/prisma/__mocks__/prisma'
+// import prisma from 'src/prisma/__mocks__/prisma'
 
 
 import { StatisticsService } from './statistics.service'; 
