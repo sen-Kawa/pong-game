cd61ac9fef7e41d63594561162a2c497de13d6ae
ffreddow <88713513+ffreddow@users.noreply.github.com>
2023-09-30 10:27:18 +0000

    Comment out unresolved dependencies. This happened due to the cherry picks.

--- backend/src/app.gateway.ts
+++ backend/src/app.gateway.ts
@@ -22,8 +22,8 @@ import { AppService } from './app.service'
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private socketService: SocketService,
-    private authService: AuthService,
-    private appService: AppService
+    // private authService: AuthService,
+    // private appService: AppService
   ) {}
 
   @WebSocketServer() public server: Server
@@ -35,36 +35,36 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 
   async handleConnection(client: Socket, ...args: any[]) {
     //TODO error handling
-    if (!client.handshake.headers.cookie) client.disconnect()
-    else {
-      try {
-        const { 'auth-cookie': token } = parse(client.handshake.headers.cookie)
-        if (token) {
-          const test = await this.authService.verifyJwt(token)
-          console.log(test)
-          if (!test) return client.disconnect()
-          this.appService.connectedUser(test.userId)
-          args
-          this.logger.log('Client connected ' + client.id)
-        }
-      } catch (error) {
-        client.disconnect()
-        console.log(error)
-      }
-    }
+    // if (!client.handshake.headers.cookie) client.disconnect()
+    // else {
+    //   try {
+    //     const { 'auth-cookie': token } = parse(client.handshake.headers.cookie)
+    //     if (token) {
+    //       // const test = await this.authService.verifyJwt(token)
+    //       // console.log(test)
+    //       if (!test) return client.disconnect()
+    //       // this.appService.connectedUser(test.userId)
+    //       args
+    //       this.logger.log('Client connected ' + client.id)
+    //     }
+    //   } catch (error) {
+    //     client.disconnect()
+    //     console.log(error)
+    //   }
+    // }
   }
 
   async handleDisconnect(client: Socket) {
     //TODO error handling
-    try {
-      const { 'auth-cookie': token } = parse(client.handshake.headers.cookie)
-      if (token) {
-        const test = await this.authService.verifyJwt(token)
-        this.appService.disconnectedUser(test.userId)
-        this.logger.log('Client disconnected ' + client.id)
-      }
-    } catch (error) {
-      console.log(error)
-    }
+    // try {
+    //   const { 'auth-cookie': token } = parse(client.handshake.headers.cookie)
+    //   if (token) {
+    //     // const test = await this.authService.verifyJwt(token)
+    //     // this.appService.disconnectedUser(test.userId)
+    //     this.logger.log('Client disconnected ' + client.id)
+    //   }
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,9 +8,9 @@ import { validate } from './config/env.validation'
 import { MulterModule } from '@nestjs/platform-express'
 import { SocketModule } from './socket/socket.module'
 import { AppGateway } from './app.gateway'
-import { StatisticsModule } from './statistics/statistics.module'
-import { ChatsModule } from './khrov-chat/chats/chats.module'
-import { ChannelsModule } from './khrov-chat/channels/channels.module'
+// import { StatisticsModule } from './statistics/statistics.module'
+// import { ChatsModule } from './khrov-chat/chats/chats.module'
+// import { ChannelsModule } from './khrov-chat/channels/channels.module'
 import { AppService } from './app.service'
 
 @Module({
