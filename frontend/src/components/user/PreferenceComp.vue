e47e5d4c055896e54ea011b735624cb9f2dcdf68
olin_tp_ar <universalexperimental@protonmail.com>
2023-09-30 15:33:16 +0200

    subtitle class for welcome username

--- frontend/src/components/user/PreferenceComp.vue
+++ frontend/src/components/user/PreferenceComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center">Welcome {{ getUserName }}</div>
+  <div class="subtitle">Welcome {{ getUserName }}</div>
   <div class="text-center">change user name</div>
 
   <img v-bind:src="avatarUrl" width="50" height="60" />
