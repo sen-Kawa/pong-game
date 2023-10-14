296c59631254296cade296835ace07006669ee98
chelmerd <chelmerd@student.42wolfsburg.de>
2023-10-14 14:17:43 +0200

    add leave queue logic

--- frontend/src/components/match/WaitingRoom.vue
+++ frontend/src/components/match/WaitingRoom.vue
@@ -7,13 +7,38 @@
 <script setup lang="ts">
 import router from '@/router'
 import { socket } from '@/sockets/sockets'
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+import { onBeforeRouteLeave } from 'vue-router'
+
+const authStore = useAuthStore()
+const canLeave = ref(false)
+const shouldLeaveQueue = ref(true)
+
+onBeforeRouteLeave(() => {
+  if (canLeave.value) {
+    if (shouldLeaveQueue.value) sendLeaveQueueEvent()
+    return true
+  }
+  const answer = window.confirm('Do yo really want to leave the queue?')
+  console.debug(answer)
+  if (!answer) return false
+  sendLeaveQueueEvent()
+})
+
+function sendLeaveQueueEvent() {
+  socket.emit('leaveQueue', { userId: authStore.getUserId })
+}
 
 function leaveQueue() {
-  socket.emit('leaveQueue')
+  canLeave.value = true
   router.back()
 }
 
 socket.on('newGame', (matchId: number) => {
   console.log(`joining match ${matchId}`)
+  canLeave.value = true
+  shouldLeaveQueue.value = false
+  // TODO: redirect to the game
 })
 </script>
